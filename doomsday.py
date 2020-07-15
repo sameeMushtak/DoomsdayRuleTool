a447cf24d0fbd4ac9167daed054f60fd240b31f6
sameeMushtak <33213896+sameeMushtak@users.noreply.github.com>
2020-07-15 12:10:57 -0400

    Added calendar function

--- doomsday.py
+++ doomsday.py
@@ -1,193 +1,34 @@
-import random
-import time
-import sys
+import doomsday_funcs as df
 
 # Calculate statistics (percent correct, average time) for last n attempts
 # Leaderboard of record times
 # Code assumes existence of a doomsday_stats.csv file with header row already existing
 
-days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-months = ["January", "February", "March", "April", "May", "June",\
-        "July", "August", "September", "October", "November", "December"]
-
-class Settings:
-    def __init__(self, min_year=1583, max_year=2500, saving=True):
-        self.min_year = min_year
-        self.max_year = max_year
-        self.saving = saving
-    def set_min_year(self, min_year):
-        self.min_year = min_year
-    def set_max_year(self, max_year):
-        self.max_year = max_year
-    def set_saving(self, saving):
-        self.saving = saving
-    def saving_string(self):
-        return "On" if self.saving else "Off"
-
-config = Settings()
-
-# Assumes that a valid date is passed in.
-# BCE is probably problematic.
-def day_of_the_week(year, month, day):
-    anchor = 2 + year + (year // 4) - (year // 100) + (year // 400)
-    is_leapyear = (year%4 == 0) and ((year%100 != 0) or (year%400 == 0))
-    day_zero = [4, 0, 0, 3, 5, 1, 3, 6, 2, 4, 0, 2]
-    if is_leapyear:
-        day_zero[0] = 3
-        day_zero[1] = 6
-    ans = (anchor + day_zero[month-1] + day) % 7
-    return ans
-
-def eng_date(year, month, day):
-    return f"{months[month-1]} {day}, {year}"
-
-def practice():
-    while True:
-        # Randomly generate a date
-        year = random.randint(config.min_year,config.max_year)
-        is_leapyear = (year%4 == 0) and ((year%100 != 0) or (year%400 == 0))
-        len_months = [31, 28+is_leapyear, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-        month = random.randint(1,12)
-        day = random.randint(1,len_months[month-1])
-
-        # Display date
-        print(eng_date(year,month,day))
-        ans = day_of_the_week(year,month,day)
-        # Measuring time taken for user response
-        tic = time.perf_counter()
-        user_ans = int(input())
-        toc = time.perf_counter()
-        # Display answer and time
-        print(f"Correct Answer: {days[ans]} ({ans})")
-        print(f"Elapsed Time: {toc-tic:.1f} s")
-
-        # Store statistics in a file
-        is_correct = ans == user_ans
-        N = 0
-        if config.saving:
-            with open("doomsday_stats.csv", "r+") as f:
-                for line in f:
-                    N += 1
-                print(f"{N}, {is_correct}, {toc-tic:.3f}", file=f)
-
-        # Go to next page
-        while True:
-            next_page = input("Continue [c], Return to Main Menu [m], or Exit [e] ")
-            if next_page.lower() == "c":
-                break
-            elif next_page.lower() == "m":
-                return None
-            elif next_page.lower() == "e":
-                exit_program()
-            else:
-                print("Invalid Option")
-
-def view_stats():
-    print("Statistics")
-    with open("doomsday_stats.csv", "r") as f:
-        print(f.read())
-
-    while True:
-        next_page = input("Return to Main Menu [m], or Exit [e] ")
-        if next_page.lower() == "m":
-            return None
-        elif next_page.lower() == "e":
-            exit_program()
-        else:
-            print("Invalid Option")
-
-def reset_stats():
-    confirm = input("Confirm reset statistics [y/n] ")
-    if confirm.lower() == "y":
-        with open("doomsday_stats.csv", "w") as f:
-            print("Problem Number, Correct?, Time (s)", file=f)
-        print("Statistics Reset, Returning to Main Menu")
-    else:
-        print("Returning to Main Menu")
-    return None
-
-def tutorial():
-    print("You will be presented with a date in the format Month Day, Year. Your task is to determine the day of the week on which that date occurred. Your answer should be a single-digit number as follows:")
-    print("0: Sunday")
-    print("1: Monday")
-    print("2: Tuesday")
-    print("3: Wednesday")
-    print("4: Thursday")
-    print("5: Friday")
-    print("6: Saturday")
-    while True:
-        next_page = input("Return to Main Menu [m], or Exit [e] ")
-        if next_page.lower() == "m":
-            return None
-        elif next_page.lower() == "e":
-            exit_program()
-        else:
-            print("Invalid Option")
-
-def settings():
-    while True:
-        print("Settings")
-        print("Set range of years [s]")
-        print("Save results and times from practice [y/n]")
-        print("Return to Main Menu [m], or Exit [e]")
-        next_page = input()
-        if next_page.lower() == "s":
-            while True:
-                try:
-                    # min_year = int(input("Minimum year?"))
-                    config.set_min_year(int(input("Minimum year?")))
-                    if config.min_year < 1583:
-                        raise ValueError
-                    break
-                except ValueError:
-                    print("The Gregorian calendar began in October 1582. Please input an integer that is at least 1583.")
-            while True:
-                try:
-                    config.set_max_year(int(input("Maximum year?")))
-                    # max_year = int(input("Maximum year?"))
-                    if config.max_year < config.min_year:
-                        raise ValueError
-                    break
-                except ValueError:
-                    print(f"Please input a year greater than the minimum year ({config.min_year}).")
-        elif next_page.lower() == "y":
-            config.set_saving(True)
-            print("Results and times will be saved")
-        elif next_page.lower() == "n":
-            config.set_saving(False)
-            print("Results and times will not be saved")
-        elif next_page.lower() == "m":
-            return None
-        elif next_page.lower() == "e":
-            exit_program()
-        else:
-            print("Invalid Option")
-
-def exit_program():
-    sys.exit("Exiting.")
-
 # Consider consequences of having this always be true
 while True:
     print("Begin Practice [p]")
+    print("Calendar [c]")
     print("View Statistics [v]")
     print("Reset Statistics [r]")
     print("Tutorial [t]")
     print("Settings [s]")
     print("Exit [e]")
-    print(f"Year Range: {config.min_year}-{config.max_year}")
-    print(f"Saving: {config.saving_string()}")
+    print(f"Year Range: {df.get_min_year()}-{df.get_max_year()}")
+    print(f"Saving: {df.get_saving()}")
     next_page = input()
     if next_page.lower() == "p":
-        practice()
+        df.practice()
+    elif next_page.lower() == "c":
+        df.calendar()
     elif next_page.lower() == "v":
-        view_stats()
+        df.view_stats()
     elif next_page.lower() == "r":
-        reset_stats()
+        df.reset_stats()
     elif next_page.lower() == "t":
-        tutorial()
+        df.tutorial()
     elif next_page.lower() == "s":
-        settings()
+        df.settings()
     elif next_page.lower() == "e":
-        exit_program()
+        df.exit_program()
     else:
         print("Invalid Option")
---
+++ doomsday_funcs.py
@@ -0,0 +1,235 @@
+import random
+import time
+import sys
+import re
+
+days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+months = ["January", "February", "March", "April", "May", "June",\
+        "July", "August", "September", "October", "November", "December"]
+
+# Class to hold values that can be changed during runtime from settings menu
+class Settings:
+    def __init__(self, min_year=1583, max_year=2500, saving=True):
+        self.min_year = min_year
+        self.max_year = max_year
+        self.saving = saving
+    def set_min_year(self, min_year):
+        self.min_year = min_year
+    def set_max_year(self, max_year):
+        self.max_year = max_year
+    def set_saving(self, saving):
+        self.saving = saving
+    def saving_string(self):
+        return "On" if self.saving else "Off"
+
+config = Settings()
+
+def get_min_year():
+    return config.min_year
+
+def get_max_year():
+    return config.max_year
+
+def get_saving():
+    return config.saving_string()
+
+def is_leap(year):
+    return (year%4 == 0) and ((year%100 != 0) or (year%400 == 0))
+
+# What to do if month is not integer between 1 and 12?
+def days_in_month(year,month):
+    len_months = [31, 28+is_leap(year), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    return len_months[month-1]
+
+# Assumes year, month, and day are integers
+def valid_date(year,month,day,min_year=0):
+    return year > min_year and month >= 1 and month <= 12 and day >= 1 and day <= days_in_month(year,month)
+
+# Assumes that a valid date is passed in.
+# BCE is probably problematic.
+def day_of_the_week(year, month, day):
+    # return date(year,month,day).isoweekday() % 7
+    anchor = 2 + year + (year // 4) - (year // 100) + (year // 400)
+    day_zero = [4, 0, 0, 3, 5, 1, 3, 6, 2, 4, 0, 2]
+    if is_leap(year):
+        day_zero[0] = 3
+        day_zero[1] = 6
+    ans = (anchor + day_zero[month-1] + day) % 7
+    return ans
+
+# What if date is invalid?
+def eng_date(year, month, day):
+    if valid_date(year,month,day):
+        return f"{months[month-1]} {day}, {year}"
+    # else:
+    #
+
+def parse_date(string):
+    # Returns year, month, day
+    iso_format = re.compile("^\d{4,4}-\d{1,2}-\d{1,2}$")
+    eng_format = re.compile("^\d{1,2}/\d{1,2}/\d{4,4}$")
+    if iso_format.match(string) != None:
+        lst = [int(x) for x in string.split("-")]
+        return lst[0], lst[1], lst[2]
+    elif eng_format.match(string) != None:
+        lst = [int(x) for x in string.split("/")]
+        return lst[2], lst[1], lst[0]
+    else:
+        return None
+
+def practice():
+    while True:
+        # Randomly generate a date
+        year = random.randint(config.min_year,config.max_year)
+        month = random.randint(1,12)
+        day = random.randint(1,days_in_month(year,month))
+
+        # Display date
+        print(eng_date(year,month,day))
+        ans = day_of_the_week(year,month,day)
+        # Measuring time taken for user response
+        tic = time.perf_counter()
+        while True:
+            try:
+                user_ans = int(input())
+                if user_ans < 0 or user_ans > 6:
+                    raise ValueError
+                break
+            except ValueError:
+                print("Input should be an integer between 0 and 6.")
+        toc = time.perf_counter()
+        # Display answer and time
+        print(f"Correct Answer: {days[ans]} ({ans})")
+        print(f"Elapsed Time: {toc-tic:.1f} s")
+
+        # Store statistics in a file
+        is_correct = ans == user_ans
+        N = 0
+        if config.saving:
+            with open("doomsday_stats.csv", "r+") as f:
+                for line in f:
+                    N += 1
+                print(f"{N}, {is_correct}, {toc-tic:.3f}", file=f)
+
+        # Go to next page
+        while True:
+            next_page = input("Continue [c], Return to Main Menu [m], or Exit [e] ")
+            if next_page.lower() == "c":
+                break
+            elif next_page.lower() == "m":
+                return None
+            elif next_page.lower() == "e":
+                exit_program()
+            else:
+                print("Invalid Option")
+
+# Calculates the day of the week given the date as a string
+def calendar():
+    while True:
+        try:
+            date_str = input("Enter a date in YYYY-MM-DD or DD/MM/YYYY format. ")
+            year, month, day = parse_date(date_str)
+            if not valid_date(year,month,day,1538):
+                raise ValueError
+            print(day_of_the_week(year,month,day))
+        except TypeError:
+            print("Not proper format")
+        except ValueError:
+            print("Not a valid date")
+        while True:
+            next_page = input("Continue with another date [c], Return to Main Menu [m], or Exit [e] ")
+            if next_page.lower() == "c":
+                break
+            elif next_page.lower() == "m":
+                return None
+            elif next_page.lower() == "e":
+                exit_program()
+            else:
+                print("Invalid Option")
+
+# Displays times and correctness statistics from practices
+def view_stats():
+    print("Statistics")
+    with open("doomsday_stats.csv", "r") as f:
+        print(f.read())
+
+    while True:
+        next_page = input("Return to Main Menu [m], or Exit [e] ")
+        if next_page.lower() == "m":
+            return None
+        elif next_page.lower() == "e":
+            exit_program()
+        else:
+            print("Invalid Option")
+
+def reset_stats():
+    confirm = input("Confirm reset statistics [y/n] ")
+    if confirm.lower() == "y":
+        with open("doomsday_stats.csv", "w") as f:
+            # Resets statistics file to contain only a header
+            print("Problem Number, Correct?, Time (s)", file=f)
+        print("Statistics Reset, Returning to Main Menu")
+    else:
+        print("Returning to Main Menu")
+    return None
+
+# Add more pages (Next page [n])
+def tutorial():
+    print("You will be presented with a date in the format Month Day, Year. Your task is to determine the day of the week on which that date occurred. Your answer should be a single-digit number as follows:")
+    print("0: Sunday")
+    print("1: Monday")
+    print("2: Tuesday")
+    print("3: Wednesday")
+    print("4: Thursday")
+    print("5: Friday")
+    print("6: Saturday")
+    while True:
+        next_page = input("Return to Main Menu [m], or Exit [e] ")
+        if next_page.lower() == "m":
+            return None
+        elif next_page.lower() == "e":
+            exit_program()
+        else:
+            print("Invalid Option")
+
+def settings():
+    while True:
+        print("Settings")
+        print("Set range of years [s]")
+        print("Save results and times from practice [y/n]")
+        print("Return to Main Menu [m], or Exit [e]")
+        next_page = input()
+        # Setting year range
+        if next_page.lower() == "s":
+            while True:
+                try:
+                    config.set_min_year(int(input("Minimum year? ")))
+                    if config.min_year < 1583:
+                        raise ValueError
+                    break
+                except ValueError:
+                    print("The Gregorian calendar began in October 1582. Please input an integer that is at least 1583.")
+            while True:
+                try:
+                    config.set_max_year(int(input("Maximum year? ")))
+                    if config.max_year < config.min_year:
+                        raise ValueError
+                    break
+                except ValueError:
+                    print(f"Please input a year greater than the minimum year ({config.min_year}).")
+        # Setting whether saving will occur
+        elif next_page.lower() == "y":
+            config.set_saving(True)
+            print("Results and times will be saved")
+        elif next_page.lower() == "n":
+            config.set_saving(False)
+            print("Results and times will not be saved")
+        elif next_page.lower() == "m":
+            return None
+        elif next_page.lower() == "e":
+            exit_program()
+        else:
+            print("Invalid Option")
+
+def exit_program():
+    sys.exit("Exiting.")
